6883706f8ebeff0b7e4141ba6a96024101c1bf19
J A <jarata@gmail.com>
2019-02-27 14:14:52 -0800

    cohorts and students layout built

--- routes/cohortRouter.js
+++ routes/cohortRouter.js
@@ -4,4 +4,29 @@ const knexConfig = require('../knexfile');
 
 const db = knex(knexConfig.development);
 
+// GET
+router.get('/', async (req, res) => {
+    res.send('Cohorts GET')
+});
+
+// GET :id
+router.get('/', async (req, res) => {
+    res.send('Cohorts GET/:id')
+});
+
+// POST
+router.post('/', async (req, res) => {
+    res.send('Cohorts GET/:id')
+});
+
+// PUT :id
+router.put('/:id', async (req, res) => {
+    res.send('Cohorts PUT/:id')
+});
+
+// DELETE :id
+router.delete('/:id', async (req, res) => {
+    res.send('Cohorts DELETE/:id')
+});
+
 module.exports = router;
--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -4,4 +4,29 @@ const knexConfig = require('../knexfile');
 
 const db = knex(knexConfig.development);
 
+// GET
+router.get('/', async (req, res) => {
+    res.send('Students GET')
+});
+
+// GET :id
+router.get('/', async (req, res) => {
+    res.send('Students GET/:id')
+});
+
+// POST
+router.post('/', async (req, res) => {
+    res.send('Students GET/:id')
+});
+
+// PUT :id
+router.put('/:id', async (req, res) => {
+    res.send('Students PUT/:id')
+});
+
+// DELETE :id
+router.delete('/:id', async (req, res) => {
+    res.send('Students DELETE/:id')
+});
+
 module.exports = router;
