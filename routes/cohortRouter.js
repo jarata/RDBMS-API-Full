2a68c58b9606e33c75dc22d81052c0ba3511e7f9
J A <jarata@gmail.com>
2019-02-27 16:09:19 -0800

    seed file for students created cohort endpoints finished

--- routes/cohortRouter.js
+++ routes/cohortRouter.js
@@ -20,27 +20,107 @@ router.get('/', async (req, res) => {
 
 // GET :id
 router.get('/:id', async (req, res) => {
-    res.send('Cohorts GET/:id')
+    // res.send('Cohorts GET/:id')
+    const {id} = req.params;
+    const cohort = await db('cohorts').where({id});
+    try {
+        if (cohort) {
+            res.status(200).json(cohort)
+        } else {
+            res.status(404).json({
+                message: "The cohort with the specified ID does not exist."
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: "The cohorts information could not be retrieved."
+        })
+    }
 });
 
 // GET :id/students
 router.get('/:id/students', async (req, res) => {
-    res.send('Cohorts GET:id/students')
+    // res.send('Cohorts GET:id/students')
+    const {id} = req.params;
+    const cohortStudent = await db('cohorts').where({id})
+    try {
+        if (cohortStudent) {
+            res.status(200).json(cohortStudent)
+        } else {
+            res.status(404).json({
+                error: "The cohort's students with the specified ID do not exist"
+            })
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            error: "The cohort's students information could not be retrieved"
+        })
+    }
 });
 
 // POST
 router.post('/', async (req, res) => {
-    res.send('Cohorts GET/:id')
+    // res.send('Cohorts GET/:id')
+    try {
+        const cohort = await db('cohorts').insert(req.body);
+        res.status(201).json({
+            message: "Cohort has been created",
+            cohort
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: "The cohorts information could not be retrieved."
+        })
+    }
 });
 
 // PUT :id
 router.put('/:id', async (req, res) => {
-    res.send('Cohorts PUT/:id')
+    // res.send('Cohorts PUT/:id')
+    const {id} = req.params;
+    const cohortName = req.body;
+    try {
+        const cohort = await db('cohorts').where({id}).first();
+        if (cohort) {
+            const update = await db('cohorts').where({id}).update(cohortName);
+            res.status(200).json(update)
+        } else {
+            res.status(404).json({
+                message: "The cohort with the specified ID does not exist."
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: "The cohorts information could not be retrieved."
+        })
+    }
 });
 
 // DELETE :id
 router.delete('/:id', async (req, res) => {
-    res.send('Cohorts DELETE/:id')
+    // res.send('Cohorts DELETE/:id')
+    const {id} = req.params;
+    const count = await db('cohorts').where({id}).del();
+    try {
+        if (count > 0) {
+            res.status(200).json({
+                message: "Cohort has been deleted"
+            })
+        } else {
+            res.status(404).json({
+                message: "The cohort with the specified ID does not exist."
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: "The cohort could not be removed"
+        })
+    }
 });
 
 module.exports = router;
---
+++ seeds/students.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('students').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('students').insert([
+        {id: 1, name: 'Test', cohort_id: 1},
+        {id: 2, name: 'Test 2', cohort_id: 2},
+        {id: 3, name: 'Test 3', cohort_id: 3}
+      ]);
+    });
+};
